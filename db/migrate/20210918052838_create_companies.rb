3b27e6aeafd65c14949f9df7d5bd7dcd2dee7ce9
Muhammad Hamza <mhamzajutt96@gmail.com>
2021-09-18 12:25:13 +0500

    Drop DB and create DB, run migrations.

--- db/migrate/20210918052838_create_companies.rb
+++ db/migrate/20210918052838_create_companies.rb
@@ -5,7 +5,6 @@ class CreateCompanies < ActiveRecord::Migration[6.0]
   def change
     create_table :companies do |t|
       t.string :name, null: false
-      t.string :email, null: false
       t.timestamps
     end
   end
