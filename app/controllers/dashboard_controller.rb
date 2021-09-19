55cf260325b275a1a0f2ccb55a09ea829a2239fd
Muhammad Hamza <mhamzajutt96@gmail.com>
2021-09-19 13:09:22 +0500

    Setup the views for employees.

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -5,6 +5,15 @@ class DashboardController < ApplicationController
   def dashboard
     if company_signed_in?
       @employees = current_company.employees
+    elsif employee_signed_in?
+      @activity_for_today = {
+        selected_participants: EmployeeChallenge.for_this_day
+                                                .includes(:employee, :challenge)
+                                                .where(employees: {
+                                                  company_id: current_employee.company_id
+                                                })
+      }
+    else
     end
   end
 
